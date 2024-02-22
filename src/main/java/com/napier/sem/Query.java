4adc3efa43746c6848b8a1bd2e3214135ec4cf58
Jacob <jacob@jaso5.com>
2024-02-22 13:29:54 +0000

    Added generic query

--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -1,7 +1,32 @@
 package com.napier.sem;
 
+import java.sql.Connection;
+
+/**
+ * Query interface for generic query execution in CLI
+ */
 public interface Query {
+    /**
+     * @return Name of query
+     */
+    String name();
+    /**
+     * @return Description of query
+     */
+    String description();
 
 
-    void execute();
+    /**
+     * @return True if a size N is required
+     */
+    boolean needsN();
+    /**
+     * @return True if a Region size is required
+     */
+    boolean needsRegion();
+
+    /**
+     * Executes the query using provided connection
+     */
+    void execute(Connection con, int n, Region area);
 }
