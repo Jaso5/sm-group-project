58d10a79206166712fef2546f5d911306f96dd20
Jacob <jacob@jaso5.com>
2024-02-22 13:11:59 +0000

    Added Region.java Enum

---
+++ src/main/java/com/napier/sem/Region.java
@@ -0,0 +1,13 @@
+package com.napier.sem;
+
+
+/** Region Enum
+ *  Used to pass region size information between the cli and queries
+ */
+public enum Region {
+    WORLD,
+    CONTINENT,
+    REGION,
+    COUNTRY,
+    DISTRICT,
+}
