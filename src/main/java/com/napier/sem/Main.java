b850acbcbd0e145b06922704b48dd227f16aa5b5
TheMrCake <antonlichota@gmail.com>
2024-04-11 18:40:15 +0100

    Testing and changing sqlconnector class.

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -2,6 +2,8 @@ package com.napier.sem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        MySQLConnector connect = new MySQLConnector("root", "sem", "world");
+        connect.connect();
     }
 }
--- src/main/java/com/napier/sem/MySQLConnector.java
+++ src/main/java/com/napier/sem/MySQLConnector.java
@@ -2,8 +2,20 @@ package com.napier.sem;
 
 import java.sql.*;
 
-public class MySQLConnector {
-    public static void Connect()
+public class MySQLConnector
+{
+    private final String user;
+    private final String pass;
+    private final String dbName;
+    private Connection con = null;
+
+    public MySQLConnector(String user, String pass, String dbName)
+    {
+        this.user = user;
+        this.pass = pass;
+        this.dbName = dbName;
+    }
+    public void connect()
     {
         try
         {
@@ -16,43 +28,37 @@ public class MySQLConnector {
             System.exit(-1);
         }
 
-        Connection con = null;
         int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
-                Thread.sleep(10000);
+                Thread.sleep(20000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "sem");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/" + dbName + "?allowPublicKeyRetrieval=true&useSSL=false", user, pass);
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(3000);
                 // Exit for loop
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
-        if (con != null)
-        {
-            try
-            {
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
