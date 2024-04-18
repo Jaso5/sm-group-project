3b6cbdc8e69857e3b70bf6f1fefb650b77a9eda8
TheMrCake <antonlichota@gmail.com>
2024-04-18 11:16:44 +0100

    Added more unit tests.

--- src/test/java/QueryTests.java
+++ src/test/java/QueryTests.java
@@ -10,4 +10,14 @@ public class QueryTests
     {
         assertEquals(new Query().addReg1(Region.WORLD).buildQuery(), "SELECT SUM(population) AS \"world population\" FROM country;");
     }
+    @Test
+    void AllCountriesQuery()
+    {
+        assertEquals(new Query().addReg1(Region.COUNTRY).buildQuery(), "SELECT country.code, country.name, country.continent, country.region, country.population, city.name as capital, CONCAT(ROUND((PIC.a/country.population)*100, 2), '%') AS \"%Pop in Cities\", CONCAT(100-ROUND((PIC.a/country.population)*100, 2), '%') AS \"%Pop not in Cities\" FROM country JOIN city ON (country.capital=city.id) JOIN (SELECT SUM(city.population) as a, city.countrycode as b FROM city GROUP BY city.countrycode) PIC on (PIC.b=country.code) ORDER BY population DESC ;");
+    }
+    @Test
+    void AllCitiesQuery()
+    {
+        assertEquals(new Query().addReg1(Region.CITY).buildQuery(), "SELECT city.name, city.population AS population FROM country JOIN city ON (country.code=city.countrycode) ORDER BY population DESC ;");
+    }
 }
