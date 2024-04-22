a186b1357d576f3c18603648adbfd8c337775a5f
Jacob <jacob@jaso5.com>
2024-04-22 14:45:30 +0100

    Add backend stuff

--- src/main/java/com/napier/sem/HttpAPI.java
+++ src/main/java/com/napier/sem/HttpAPI.java
@@ -3,6 +3,7 @@ package com.napier.sem;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import org.json.JSONObject;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
@@ -16,7 +17,31 @@ public class HttpAPI implements HttpHandler {
         System.out.println("Received request!");
         if ("POST".equals(http.getRequestMethod())) {
             // Handle request
-            this.sendResponse(http, 200, "TEST\n");
+            String body = http.getRequestBody().toString();
+            JSONObject obj = new JSONObject(body);
+
+            String q = obj.getString("q");
+
+            if (q.equals("m0")) {
+                // Get population of name with size
+                String name = obj.getString("e");
+                String as = obj.getString("as");
+
+
+                int population = 0;
+                sendResponse(http, 200, String.format("{pop: '%s'}", population));
+            } else if (q.equals("p0")) {
+                // Get total population
+                int population = 0;
+                sendResponse(http, 200, String.format("{pop: '%s'}", population));
+            } else {
+                // Run query Q, with extra data maybe, see two lines below
+//                String as = obj.getString("as");
+//                int N = obj.getInt("e");
+
+                String filename = "style.css";
+                sendResponse(http, 200, String.format("{file: '%s'}", filename));
+            }
         } else {
             // Error
             this.sendResponse(http, 400, "Bad Request\n");
